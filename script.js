2bea65548f46d09558f997ae601852dc0431667e
jaafarhamza <jaafar.hamza711@gmail.com>
2024-11-25 23:46:08 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// // Select the toggle button and navbar menu
+// const toggleButton = document.querySelector('[data-collapse-toggle="navbar-default"]');
+// const navbarMenu = document.getElementById('navbar-default');
+
+// // Toggle visibility of the menu on button click
+// toggleButton.addEventListener('click', () => {
+//   navbarMenu.classList.toggle('hidden'); // Show/hide the menu
+// });
+
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-gradient': "linear-gradient(180deg, rgba(124,124,124,0.15) 0%, rgba(101,101,101,0) 0%, rgba(113,113,113,0.71) 47%, rgba(101,101,101,1) 100%)",
+      },
+    },
+  },
+  plugins: [],
+}
+
