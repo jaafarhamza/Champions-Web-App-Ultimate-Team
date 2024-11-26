c85fc4b348585f44de6f5f621d0d3a02924cc904
jaafarhamza <jaafar.hamza711@gmail.com>
2024-11-26 18:20:54 +0100

    responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'custom-gradient': "linear-gradient(180deg, rgba(124,124,124,0.15) 0%, rgba(101,101,101,0) 0%, rgba(113,113,113,0.71) 47%, rgba(101,101,101,1) 100%)",
+        'custom-svg': "url('/Champions-Web-App-Ultimate-Team/assest/téléchargement.svg')",
       },
     },
   },
